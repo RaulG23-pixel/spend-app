5af074a88c9691ef2b79fa75fc5ee71f90069b0a
Raul Gonzalez <raulx12@gmail.com>
2022-07-07 18:05:32 -0430

    Seccion de Features completada

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ilustration from "../assets/ilustration1.svg";
 import ilustration2 from "../assets/ilustration2.svg";
+import icon1 from "../assets/icon1.png";
+import icon2 from "../assets/icon2.png";
+import icon3 from "../assets/icon3.png";
 import { NavLink } from "react-router-dom";
 
 function Home() {
-  window.onscroll = () => {
-    if (window.scrollY >= 300);
-  };
   return (
     <div className="landing__page">
       <header className="landing__header">
@@ -131,7 +131,7 @@ function Home() {
         >
           <path
             fill="#ffffff"
-            fill-opacity="1"
+            fillOpacity="1"
             d="M0,160L30,165.3C60,171,120,181,180,176C240,171,300,149,360,122.7C420,96,480,64,540,64C600,64,660,96,720,112C780,128,840,128,900,112C960,96,1020,64,1080,64C1140,64,1200,96,1260,122.7C1320,149,1380,171,1410,181.3L1440,192L1440,0L1410,0C1380,0,1320,0,1260,0C1200,0,1140,0,1080,0C1020,0,960,0,900,0C840,0,780,0,720,0C660,0,600,0,540,0C480,0,420,0,360,0C300,0,240,0,180,0C120,0,60,0,30,0L0,0Z"
           ></path>
         </svg>
@@ -139,26 +139,41 @@ function Home() {
           <h2>Features</h2>
           <div className="features__flex">
             <div className="feature">
+              <img src={icon1} alt="Icon feature 1" />
+
               <div>
-                <h4>feature</h4>
-                <p>Lorem</p>
+                <h4>Registro de gastos</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                  bibendum mollis diam, a dapibus lacus vehicula ac. Ut dolor
+                  mi, cursus eget tempus non.
+                </p>
               </div>
             </div>
             <div className="feature">
+              <img src={icon2} alt="Icon feature 2" />
               <div>
-                <h4>feature</h4>
-                <p>Lorem</p>
+                <h4>Listado de pendientes</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                  bibendum mollis diam, a dapibus lacus vehicula ac. Ut dolor
+                  mi, cursus eget tempus non, pellentesque eget justo.
+                </p>
               </div>
             </div>
             <div className="feature">
+              <img src={icon3} alt="Icon feature 3" />
               <div>
-                <h4>feature</h4>
-                <p>Lorem</p>
+                <h4>Visualización de estimado de ahorros</h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                  bibendum mollis diam, a dapibus lacus vehicula ac. Ut dolor
+                  mi, cursus eget tempus non, pellentesque eget justo.
+                </p>
               </div>
             </div>
           </div>
         </div>
-
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1440 320"
